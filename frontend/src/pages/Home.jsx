e8bce89af5b855bbf59819fa215c9c78e6e36689
Aayush Dutta <aayush.dutta22@st.niituniversity.in>
2025-12-15 22:00:13 +0530

    Homepage Update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,8 +8,8 @@ function Home() {
             <div className="content">
                 <div className="Introduction">
                     <h1>Welcome to My Mind</h1>
-                    <p>To Get to know me better, You can ask me anything</p>
-                    <p>just type your question in the box below</p>
+                    <span style={{ display: "flex", justifyContent: "space-around" }}><p>To Get to know me better, You can ask me anything</p>
+                        <p>just type your question in the box below</p></span>
                 </div>
                 <form action="">
                     <input id="textarea" type="text" placeholder="Type your question" />
